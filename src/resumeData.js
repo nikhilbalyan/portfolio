de0656eb30b4c5981e3d19ae296e491031bccfbc
Nikhil <nbalyan10@gmail.com>
2020-05-24 13:05:19 +0530

    add components and portfolio data

---
+++ src/resumeData.js
@@ -0,0 +1,150 @@
+let resumeData = {
+  "imagebaseurl":"http://nikhilbalyan.github.io/portfolio",
+  "name": "Nikhil",
+  "role": "Integration Professional",
+  "linkedinId":"https://www.linkedin.com/in/nikhil-990601a2/",
+  "skypeid": "Your skypeid",
+  "roleDescription": " An enthusiastic software developer with a knack of designing and developing secure, scalable and robust Mule applications.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/nikhil-990601a2/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/nikhilbalyan",
+        "className":"fa fa-github"
+      }
+      /* ,
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      } */
+    ],
+  "aboutme":"A Computer Science Engineer with more than 2 years of industry experience focusing on Integration domain spreading across various domains like Travel, Insurance and CRM ",
+  "address":"Bengaluru, India",
+  "email":"nbalyan10@gmail.com",
+  "education":[
+    {
+      "UniversityName":"Manav Rachna International University",
+      "specialization":"Computer Science and Engineering",
+      "MonthOfPassing":"July",
+      "YearOfPassing":"2017",
+      "Achievements": 'Campus Google Applied CS with Android Facilitator',
+      "Achievements1": 'Winner of programming Competition supported by IEEE Computer Society, Delhi Section',
+      "Achievements2": "Winner of Declamation Contest"
+    }
+    /* ,
+    {
+      "UniversityName":"Some University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    } */
+  ],
+  "work":[
+    {
+      "CompanyName":"Mindtree",
+      "specialization":"Software Developer",
+      "MonthOfJoining": "Nov",
+      "YearOfJoining": "2017",
+      "MonthOfLeaving":"",
+      "YearOfLeaving":"Present",
+      "Achievements":"Working on solving business problems of Integrating different systems using Mulesoft Ecosystem"
+    },
+    {
+      "CompanyName":"Softnerve",
+      "specialization":"Software Development R&D Intern",
+      "MonthOfJoining": "Sep",
+      "YearOfJoining": "2017",
+      "MonthOfLeaving":"Oct",
+      "YearOfLeaving":"2017",
+      "Achievements":"Contributed significantly for the integration of IOT devices and AWS IOT platform using Spring Boot Microservices"
+    },
+    {
+      "CompanyName":"CityExploro",
+      "specialization":"Android Developer Intern",
+      "MonthOfJoining": "Jan",
+      "YearOfJoining": "2017",
+      "MonthOfLeaving":"Jun",
+      "YearOfLeaving":"2017",
+      "Achievements":"Contributed in the development of an Android Application for exploring cities and launched the alpha version on Google Play Store"
+    },
+    {
+      "CompanyName":"R Systems International",
+      "specialization":"Android Developer Intern",
+      "MonthOfJoining": "Oct",
+      "YearOfJoining": "2015",
+      "MonthOfLeaving":"Apr",
+      "YearOfLeaving":"2016",
+      "Achievements":"Developed an Android Application for submission of cheques"
+    }
+  ],
+  "skillsDescription":"Have hands on experience on",
+  "skills":[
+    {
+      "skillname":"Mule4"
+    },
+    {
+      "skillname":"Mule3"
+    },
+    {
+      "skillname":"RAML"
+    },
+    {
+      "skillname":"Dataweave"
+    },
+    {
+      "skillname":"Spring Boot"
+    },
+    {
+      "skillname":"Java 8"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"JavaScript"
+    },
+    {
+      "skillname":"Android"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"https://www.linkedin.com/feed/update/urn:li:activity:6659077003066646528/"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
